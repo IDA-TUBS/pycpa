8ae8db2d9bf973264b0d105602d8a647a61afa2c
Philip Axer <axer@ida.ing.tu-bs.de>
2012-02-06 11:27:20 +0100

    added requirements to the installer * matplotlib, pygraphviz, argparse

--- setup.py
+++ setup.py
@@ -15,14 +15,18 @@ Setup
 """
 
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pycpa',
       version='1.0',
       description='pyCPA - a python implementation of compositional performance analysis',
       author='Jonas Diemer, Philip Axer',
       author_email='{axer, diemer}@ida.ing.tu-bs,de',
-      url='',
+      url='http://code.google.com/p/pycpa',
+      license="MIT",
       package_dir={'': 'src'},
       packages=['pycpa'],
+      install_requires=['matplotlib', 'pygraphviz', 'argparse'],
+      extras_require={'gantt_charts' : 'simpy',
+                      'doc' : 'sphinx'}
      )
