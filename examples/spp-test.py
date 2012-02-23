d9e1c18b0094222aa946e2588c36b333adc0c868
Philip Axer <axer@ida.ing.tu-bs.de>
2012-02-23 18:54:28 +0100

    pretty wcrt printing

--- examples/spp-test.py
+++ examples/spp-test.py
@@ -35,20 +35,20 @@ def spp_test():
     r2 = s.add_resource("R2", spp.w_spp, spp.spp_multi_activation_stopping_condition)
 
     # create and bind tasks to r1
-    t11 = r1.bind_task(model.Task("T11", wcet = 10, bcet = 5, scheduling_parameter = 1))
-    t12 = r1.bind_task(model.Task("T12", wcet = 3, bcet = 1, scheduling_parameter = 2))
+    t11 = r1.bind_task(model.Task("T11", wcet=10, bcet=5, scheduling_parameter=1))
+    t12 = r1.bind_task(model.Task("T12", wcet=3, bcet=1, scheduling_parameter=2))
 
     # create and bind tasks to r2
-    t21 = r2.bind_task(model.Task("T21", wcet = 2, bcet = 2, scheduling_parameter = 1))
-    t22 = r2.bind_task(model.Task("T22", wcet = 9, bcet = 4, scheduling_parameter = 2))
+    t21 = r2.bind_task(model.Task("T21", wcet=2, bcet=2, scheduling_parameter=1))
+    t22 = r2.bind_task(model.Task("T22", wcet=9, bcet=4, scheduling_parameter=2))
 
     # specify precedence constraints: T11 -> T21; T12-> T22
     t11.link_dependent_task(t21)
     t12.link_dependent_task(t22)
 
     # register a periodic with jitter event model for T11 and T12
-    t11.in_event_model = model.EventModel(P = 30, J = 5)
-    t12.in_event_model = model.EventModel(P = 15, J = 6)
+    t11.in_event_model = model.EventModel(P=30, J=5)
+    t12.in_event_model = model.EventModel(P=15, J=6)
 
     # plot the system graph to visualize the architecture
     g = graph.graph_system(s, 'spp_graph.pdf')
@@ -60,9 +60,9 @@ def spp_test():
     # print the worst case response times (WCRTs)
     print("Result:")
     print(s)
-    for r in sorted(s.resources, key = str):
-        for t in sorted(r.tasks, key = str):
-            print(str(t), " - ", t.wcrt)
+    for r in sorted(s.resources, key=str):
+        for t in sorted(r.tasks, key=str):
+            print("%s: wcrt=%d" % (t.name, t.wcrt))
 
 
 if __name__ == "__main__":
