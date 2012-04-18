4472b87c2bfa036f6fa5d2c32a878bf52a9c78ff
jonasd <unknown>
2012-04-18 14:54:59 +0200

    added option to plot ticks at steps in eta. pep8 compliant code formatting.

--- src/pycpa/plot.py
+++ src/pycpa/plot.py
@@ -37,19 +37,19 @@ def augment_range(plot_range):
 
     return a_range
 
-def plot_eta(eta, plot_range, label = None, color = None):
+def plot_eta(eta, plot_range, label=None, color=None):
     """ Plot an eta function """
     # range including surroundings of integers to get the steps right
     augmented_range = plot_range + [x + 0.0001 for x in plot_range] + [x - 0.0001 for x in plot_range]
     augmented_range.sort()
 
-    pyplot.plot(augmented_range, [eta(x) for x in augmented_range], label = label, color = color)
-    pyplot.xlim(xmin = 0)
-    pyplot.ylim(ymin = 0)
+    pyplot.plot(augmented_range, [eta(x) for x in augmented_range], label=label, color=color)
+    pyplot.xlim(xmin=0)
+    pyplot.ylim(ymin=0)
     pyplot.xlabel("$\Delta t$")
 
 
-def plot_event_model(model, num_events, file_format = None, separate_plots = True, file_prefix = ''):
+def plot_event_model(model, num_events, file_format=None, separate_plots=True, file_prefix='', ticksAtSteps=False):
     """ Plot the Task's eta and delta_min functions.
     Intervals in eta are shown half-open, as defined in [Richter2005]_.
     
@@ -68,41 +68,44 @@ def plot_event_model(model, num_events, file_format = None, separate_plots = Tru
 
     eps = 1e-10 # epsilon
     max_delta_t = model.delta_min(num_events + 1)
-    range_eta = range(max_delta_t + 1)
 
     # create ranges which have one point at each step of eta
     steps_eta_plus = [model.delta_min(x) for x in range(num_events + 1)]
     steps_eta_min = [model.delta_plus(x) for x in range(num_events + 1)]
+    steps_eta = sorted(set(steps_eta_min + steps_eta_plus))
+
+    range_eta = range(max(steps_eta_min) + 1)
 
     # range including surroundings of integers
-    augmented_range = range_eta + [x + eps for x in range_eta] + [x - eps for x in range_eta]
-    augmented_range.sort()
+    augmented_range = sorted(range_eta + [x + eps for x in range_eta] + [x - eps for x in range_eta])
     w, h = pyplot.figaspect(0.7)
-    pyplot.figure(figsize = (w, h))
+    pyplot.figure(figsize=(w, h))
 
     # plot eta functions
     if not separate_plots:
         pyplot.subplot(121)
-        pyplot.subplots_adjust(left = 0.05, bottom = 0.1, right = 0.97, top = 0.92, wspace = None, hspace = None)
+        pyplot.subplots_adjust(left=0.05, bottom=0.1, right=0.97, top=0.92, wspace=None, hspace=None)
 
     #eta minus first (so it appears below in case of overlaps)
-    pyplot.plot([0], [0], 'g-^', label = "$\eta^-(\Delta t)$") #only one point for label + legend (with line and marker)
+    pyplot.plot([0], [0], 'g-^', label="$\eta^-(\Delta t)$") #only one point for label + legend (with line and marker)
     pyplot.plot(augmented_range, [model.eta_min(x) for x in augmented_range], 'g-') # line only
     pyplot.plot(steps_eta_min, [model.eta_min(x + eps) for x in steps_eta_min], 'g^') # inclusive markers
     pyplot.plot(steps_eta_min, [model.eta_min(x) for x in steps_eta_min], 'w^') # exclusive markers
 
     #now eta plus on top    
-    pyplot.plot([0], [0], 'r-v', label = "$\eta^+(\Delta t)$") #only one point for label + legend (with line and marker)
+    pyplot.plot([0], [0], 'r-v', label="$\eta^+(\Delta t)$") #only one point for label + legend (with line and marker)
     pyplot.plot(augmented_range, [model.eta_plus(x) for x in augmented_range], 'r-') # line only
     pyplot.plot(steps_eta_plus, [model.eta_plus(x - eps) for x in steps_eta_plus], 'rv') # inclusive markers
     pyplot.plot(steps_eta_plus, [model.eta_plus(x) for x in steps_eta_plus], 'wv') # exclusive markers
 
-    pyplot.xlim(xmin = 0, xmax = max_delta_t)
-    pyplot.ylim(ymin = 0, ymax = num_events + .5)
+    pyplot.xlim(xmin=0, xmax=max_delta_t)
+    pyplot.ylim(ymin=0, ymax=num_events + .5)
+    if ticksAtSteps:
+        pyplot.xticks(steps_eta)
     pyplot.title("$\eta(\Delta t)$")
     pyplot.xlabel("$\Delta t$")
     pyplot.ylabel("$n$")
-    pyplot.legend(loc = 'best')
+    pyplot.legend(loc='best')
 
     if separate_plots and file_format is not None:
             pyplot.savefig(file_prefix + "plot-eta." + file_format)
@@ -117,20 +120,20 @@ def plot_event_model(model, num_events, file_format = None, separate_plots = Tru
 
     # plot delta_min first so it appears below when overlapping
     pyplot.plot(range_delta, [model.delta_min(x) for x in range_delta], 'r^',
-                label = "$\delta^-(n)$")
+                label="$\delta^-(n)$")
 
     # plot delta_plus on top
     if model.delta_plus(2) < float('inf'):
         # only plot delta+ if it is not infinity
         pyplot.plot(range_delta, [model.delta_plus(x) for x in range_delta], 'gv',
-                    label = "$\delta^+(n)$")
+                    label="$\delta^+(n)$")
 
-    pyplot.xlim(xmin = 0, xmax = num_events + .5)
-    pyplot.ylim(ymin = 0)
+    pyplot.xlim(xmin=0, xmax=num_events + .5)
+    pyplot.ylim(ymin=0)
     pyplot.title("$\delta(n)$")
     pyplot.xlabel("n")
     pyplot.ylabel("$\Delta t$")
-    pyplot.legend(loc = 'best')
+    pyplot.legend(loc='best')
 
     if file_format is not None:
         if separate_plots:
@@ -140,7 +143,7 @@ def plot_event_model(model, num_events, file_format = None, separate_plots = Tru
     else:
         pyplot.show()
 
-def aesthetic_paper_parameters(column_size = 252):
+def aesthetic_paper_parameters(column_size=252):
     fig_width_pt = column_size  # Get this from LaTeX using \showthe\columnwidth
     inches_per_pt = 1.0 / 72.27               # Convert pt to inch
     golden_mean = (math.sqrt(5) - 1.0) / 2.0         # Aesthetic ratio
@@ -159,31 +162,31 @@ def aesthetic_paper_parameters(column_size = 252):
     print params
     return params
 
-def plot_gantt(tasks, file_name = None, show = True, xlim = None,
-               preemtion_bar_height = 0.2,
-               height = 1, #height of the box during actual execution
-               hdist = 1, #vertical distance between two execution bars 
-               min_dist_arrows = 0.2, # minimum distance between arrows (e.g. in case actications overlap
-               plot_event_arrival = True, # plot arrival arrows
-               plot_activation_finishing = False, # plot finishing arrows
-               annotate_tasks = True, # annotate additional information
-               task = None, # the task that should be annotated (draws a WCRT arrow for this task)
-               wcrt_voffset = 0.5, # amount of vertical distance used for the wcrt arrow
-               annotation_offset = 0.2, # offset of the annotation (q=1) text
-               arrow_width = 0.05, # arrow width
-               arrow_head_width = 0.4,
-               arrow_head_length = 0.2,
-               arrow_yoffset = 0.1, # arrow vertical offset
-               xticks_only_on_changes = False, # place tick only when events happen
-               color_preemtion_bar = '0.30',
-               color_execution_bar = 'yellow'):
+def plot_gantt(tasks, file_name=None, show=True, xlim=None,
+               preemtion_bar_height=0.2,
+               height=1, #height of the box during actual execution
+               hdist=1, #vertical distance between two execution bars 
+               min_dist_arrows=0.2, # minimum distance between arrows (e.g. in case actications overlap
+               plot_event_arrival=True, # plot arrival arrows
+               plot_activation_finishing=False, # plot finishing arrows
+               annotate_tasks=True, # annotate additional information
+               task=None, # the task that should be annotated (draws a WCRT arrow for this task)
+               wcrt_voffset=0.5, # amount of vertical distance used for the wcrt arrow
+               annotation_offset=0.2, # offset of the annotation (q=1) text
+               arrow_width=0.05, # arrow width
+               arrow_head_width=0.4,
+               arrow_head_length=0.2,
+               arrow_yoffset=0.1, # arrow vertical offset
+               xticks_only_on_changes=False, # place tick only when events happen
+               color_preemtion_bar='0.30',
+               color_execution_bar='yellow'):
     """ Plot a gantt chart of a given task list.
         Execution time information is taken from the task attribute q_exec_windows which is written by the simulation framework
     """
     #matplotlib.rcParams.update(aesthetic_paper_parameters(column_size = 2 * 252))
 
     w, h = pyplot.figaspect(0.4)
-    fig = pyplot.figure(figsize = (w * 0.5, h * 0.5))
+    fig = pyplot.figure(figsize=(w * 0.5, h * 0.5))
     ax = fig.add_subplot(111)
 
     ypos = 0
@@ -225,8 +228,8 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
         # draw the bars verically aligned to ypos, so that ypos is the middle of the bar
         ax.broken_barh(parts,
                        (ypos - height / 2. , height),
-                       facecolors = color_execution_bar,
-                       alpha = 1)
+                       facecolors=color_execution_bar,
+                       alpha=1)
 
         # draw preemtion times, activation and response time arrows
         xposshift = 0
@@ -242,10 +245,10 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
 
             ax.broken_barh([(xpos, task_activity_end - xpos)],
                            (ypos - preemtion_bar_height / 2. , preemtion_bar_height),
-                           facecolors = color_preemtion_bar,
-                           edgecolor = color_preemtion_bar,
-                           alpha = 1,
-                           zorder = 0)
+                           facecolors=color_preemtion_bar,
+                           edgecolor=color_preemtion_bar,
+                           alpha=1,
+                           zorder=0)
 
             # check if there are multiple activations right after each other
             # we don't want them to overlap, s
@@ -259,12 +262,12 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
                 # Draw activation times
                 activation_arrow = patches.FancyArrow(xpos + xposshift, ypos + arrow_yoffset,
                                                       0, height / 2. ,
-                                                      length_includes_head = True,
-                                                      width = arrow_width,
-                                                      head_width = arrow_head_width,
-                                                      head_length = arrow_head_length,
-                                                      facecolor = 'black',
-                                                      alpha = 1)
+                                                      length_includes_head=True,
+                                                      width=arrow_width,
+                                                      head_width=arrow_head_width,
+                                                      head_length=arrow_head_length,
+                                                      facecolor='black',
+                                                      alpha=1)
                 ax.add_patch(activation_arrow)
 
             if plot_activation_finishing == True:
@@ -272,12 +275,12 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
                 q_wcrt = t.q_exec_windows[q][-1][1]
                 response_arrow = patches.FancyArrow(q_wcrt, ypos - arrow_yoffset,
                                                     0, -height / 2. ,
-                                                    length_includes_head = True,
-                                                    width = arrow_width,
-                                                    head_width = arrow_head_width,
-                                                    head_length = arrow_head_length,
-                                                    facecolor = 'black',
-                                                    alpha = 1)
+                                                    length_includes_head=True,
+                                                    width=arrow_width,
+                                                    head_width=arrow_head_width,
+                                                    head_length=arrow_head_length,
+                                                    facecolor='black',
+                                                    alpha=1)
                 ax.add_patch(response_arrow)
 
             if annotate_tasks == True and (task and task == t):
@@ -293,8 +296,8 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
                 #     verticalalignment = 'bottom')
 
                 ax.text(first_segment + annotation_offset, ypos - height / 2., text,
-                     horizontalalignment = 'left',
-                     verticalalignment = 'bottom')
+                     horizontalalignment='left',
+                     verticalalignment='bottom')
 
             if task and task == t:
                 wcrt_start = t.in_event_model.delta_min(t.q_max)
@@ -302,13 +305,13 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
                 annotation_ypos = ypos - height / 2. - wcrt_voffset
                 wcrt_arrow = patches.FancyArrowPatch((wcrt_start, annotation_ypos),
                                                      (wcrt_end, annotation_ypos),
-                                                      arrowstyle = "<->", mutation_scale = 20.)
+                                                      arrowstyle="<->", mutation_scale=20.)
                 ax.add_patch(wcrt_arrow)
 
                 ax.text(wcrt_start + t.wcrt / 2., annotation_ypos, 'WCRT=%f' % (t.wcrt),
-                        horizontalalignment = 'center',
-                        verticalalignment = 'center',
-                        backgroundcolor = 'white')
+                        horizontalalignment='center',
+                        verticalalignment='center',
+                        backgroundcolor='white')
 
 
 
@@ -322,17 +325,17 @@ def plot_gantt(tasks, file_name = None, show = True, xlim = None,
     if xticks_only_on_changes == True:
         ax.set_xticks(xticks)
     else:
-        ax.xaxis.set_major_locator(ticker.MaxNLocator(20, steps = [1, 2, 5]))
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(20, steps=[1, 2, 5]))
 
     ax.set_yticklabels([t.name for t in tasks]) # set tasknames
-    ax.autoscale_view(tight = True, scalex = True, scaley = True) # autoscale first
+    ax.autoscale_view(tight=True, scalex=True, scaley=True) # autoscale first
     ax.set_ylim(ypos + hdist + height / 2. , height) # set ylim manual
     if xlim:
         ax.set_xlim(-arrow_head_width, xlim)
     ax.grid(True)
 
     if file_name is not None:
-        pyplot.savefig(file_name, bbox_inches = 'tight')
+        pyplot.savefig(file_name, bbox_inches='tight')
 
     if show:
         pyplot.show()
