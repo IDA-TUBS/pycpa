ab48ee2426d12b9c01bbd6f8384d45953a032c9e
jonasd <unknown>
2012-05-11 16:50:39 +0200

    added example of event model plot

---
+++ examples/plot_event_model.py
@@ -0,0 +1,33 @@
+"""
+| Copyright (C) 2010 Philip Axer
+| TU Braunschweig, Germany
+| All rights reserved. 
+| See LICENSE file for copyright and license details.
+
+:Authors:
+         - Jonas Diemer
+
+Description
+-----------
+
+Plot an event model.
+"""
+
+import logging
+
+
+from pycpa import model
+from pycpa import plot
+from pycpa import options
+
+P = 250
+J = 250#5
+d = 0#1
+em = model.EventModel(P=P, J=J, dmin=d)
+
+
+print "delta_min(0) =", em.delta_min(0)
+print "eta_plus(0) =", em.eta_plus(0)
+print "eta_plus(eps) =", em.eta_plus(1e-12)
+
+plot.plot_event_model(em, 5, separate_plots=False, file_format='pdf', file_prefix='event-model-', ticks_at_steps=True)
