34b250aaba23e8862e4ac0720fab7299864144f2
jonasd <unknown>
2012-06-04 14:54:31 +0200

    fixed closed interval eta_plus for w=0

--- src/pycpa/model.py
+++ src/pycpa/model.py
@@ -165,8 +165,6 @@ class EventModel (object):
             but assuming CLOSED intervals for w
             as defined in [Richter2005]_.      
         """
-        # the window for 0 activations is 0
-        if w <= 0: return 0
         # if the window does not include 2 activations, assume that one has occured        
         if self.delta_min(2) > w: return 1
         # if delta_min is constant zero, eta_plus is always infinity
