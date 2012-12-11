244c801dea3ae8cff3cf9f0916256472928522ca
Philip Axer <axer@ida.ing.tu-bs.de>
2012-12-11 10:06:43 +0100

    reworked examples batch test to work with nosetest

--- test/test_examples.py
+++ test/test_examples.py
@@ -12,49 +12,19 @@ Description
 -----------
 
 Regression test of all examples
+This file is invoked by nosetest
 """
 
-import unittest
 import subprocess
 import glob
-import runpy
-import sys
 
-class ExamplesTest(unittest.TestCase):
-    def __init__(self, f, fsock=sys.stdout):
-        unittest.TestCase.__init__(self)
-        self.file = f
-        self.fsock = fsock
-        self._testMethodDoc = "Run example " + f
-
-    def runTest(self):
-        # call, pro: no outputs, con: no exceptions
-        self.fsock.write("######################################################")
-        self.fsock.write("# RUNNING EXAMPLE: %s" % self.file)
-        self.fsock.write("######################################################")
-
-        retval = subprocess.check_call(['python', self.file], stderr = self.fsock, stdout = self.fsock)
-
-        self.fsock.write("######################################################")
-        # like above:
-        #retval = subprocess.call(['python', self.file], stderr = subprocess.PIPE, stdout = subprocess.PIPE)
-        #self.assertEqual(retval, 0)
-
-        # runs files directly, pro: we get exceptions, con: we get all outpus
-        #runpy.run_path(self.file)
-
-
-if __name__ == "__main__":
-
+def test_examples():
+    """ run examples in a nose generator test
+    """
     directory = "../examples/"
-
-
-    suite = unittest.TestSuite()
-    fsock = open('examples.log', 'w')
-
     for e in sorted(glob.glob(directory + '*.py')):
-        suite.addTest(ExamplesTest(e, fsock))
+        yield run_example, e
 
-    unittest.TextTestRunner(verbosity = 2).run(suite)
-
-    fsock.close()
+def run_example(example):
+    retval = subprocess.check_call(['python', example])
+    assert retval == 0
