40377feafffacb03951eb11cc653696971d4f3be
Jonas Diemer <diemer@ida.ing.tu-bs.de>
2013-01-11 16:48:50 +0100

    adding mock class for documentation on readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -216,3 +216,29 @@ man_pages = [
     ('index', 'pycpa', u'pycpa Documentation',
      [u'Diemer, Axer'], 1)
 ]
+
+# Mock missing modules for automatic building on readthedocs.org.
+# see https://read-the-docs.readthedocs.org/en/latest/faq.html
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['twisted.web']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
