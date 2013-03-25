c55ca443be136311d427e80dbca5b5562d32b404
Jonas Diemer <diemer@ida.ing.tu-bs.de>
2013-03-25 14:36:32 +0100

    remove deprecated RPC calls

--- examples/xmlrpc_client.py
+++ examples/xmlrpc_client.py
@@ -44,19 +44,19 @@ try:
     t21 = proxy.new_task(r2, "t21")
     t22 = proxy.new_task(r2, "t22")
 
-    proxy.set_task_parameter(t11, "wcet", 10)
-    proxy.set_task_parameter(t11, "bcet", 5)
-    proxy.set_task_parameter(t12, "wcet", 3)
-    proxy.set_task_parameter(t12, "bcet", 1)
-    proxy.set_task_parameter(t21, "wcet", 2)
-    proxy.set_task_parameter(t21, "bcet", 2)
-    proxy.set_task_parameter(t22, "wcet", 9)
-    proxy.set_task_parameter(t22, "bcet", 4)
+    proxy.set_attribute(t11, "wcet", 10)
+    proxy.set_attribute(t11, "bcet", 5)
+    proxy.set_attribute(t12, "wcet", 3)
+    proxy.set_attribute(t12, "bcet", 1)
+    proxy.set_attribute(t21, "wcet", 2)
+    proxy.set_attribute(t21, "bcet", 2)
+    proxy.set_attribute(t22, "wcet", 9)
+    proxy.set_attribute(t22, "bcet", 4)
 
-    proxy.set_task_parameter(t11, "scheduling_parameter", 1)
-    proxy.set_task_parameter(t12, "scheduling_parameter", 2)
-    proxy.set_task_parameter(t21, "scheduling_parameter", 1)
-    proxy.set_task_parameter(t22, "scheduling_parameter", 2)
+    proxy.set_attribute(t11, "scheduling_parameter", 1)
+    proxy.set_attribute(t12, "scheduling_parameter", 2)
+    proxy.set_attribute(t21, "scheduling_parameter", 1)
+    proxy.set_attribute(t22, "scheduling_parameter", 2)
 
     proxy.link_task(t11, t21)
     proxy.link_task(t12, t22)
@@ -71,7 +71,7 @@ try:
 
     tasks = [t11, t12, t21, t22]
     for t in tasks:
-        print "results for " + proxy.get_task_parameter(t, "name")
+        print "results for " + proxy.get_attribute(t, "name")
         print proxy.get_task_result(results, t)
 except Exception, v:
     print v
