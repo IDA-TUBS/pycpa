cd4f7ad2cc040429a46189f59e0c6ee291c625e4
Philip Axer <axer@ida.ing.tu-bs.de>
2013-05-14 11:01:41 +0200

    PJd Test

--- test/event_model_tests.py
+++ test/event_model_tests.py
@@ -29,6 +29,12 @@ class Test(unittest.TestCase):
         self.assertEqual([em_b.delta_min(n) for n in seq], [em_a.delta_min(n) for n in seq])
         self.assertEqual([em_b.delta_plus(n) for n in seq], [em_a.delta_plus(n) for n in seq])
 
+    def test_delta_min_PJd(self):
+        delta_reference = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 201, 211, 221, 231, 241, 251, 261, 271, 281, 291, 301, 311, 321, 331, 341, 351, 361, 371, 381, 391, 401, 411, 421, 431, 441, 451, 461, 471, 481, 491, 501, 511, 521, 531, 541, 551, 561, 571, 581, 591, 601, 611, 621, 631, 641, 651, 661, 671, 681, 691, 701, 711, 721, 731, 741, 751, 761, 771, 781, 791, 801, 811, 821, 831, 841, 851, 861, 871, 881]
+        em = model.PJdEventModel(P=10,J=99)
+        self.assertEqual(delta_reference, [em.delta_min(n) for n in range(0,100,1)])
+
+
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
