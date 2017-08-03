6de4e6d3497be55af40d4f8e01d370e691282c14
Mischa Möstl <moestl@ida.ing.tu-bs.de>
2017-08-03 15:57:07 +0200

    Adds Amalthea to NetworkX parsing and conversion to pyCPA system 	- Two classes in nxamalthea.py facilitate this 	- An example how this works is in nx_test.py

---
+++ examples/nx_test.py
@@ -0,0 +1,27 @@
+from pycpa import util
+from pycpa import model
+from pycpa import schedulers
+from pycpa import analysis
+from pycpa import nxamalthea as nxamp
+
+
+def test():
+    amp = nxamp.NxAmaltheaParser('../data/Waters_Challenge.xml', scale=0.7)
+
+    G = amp.parse_runnables_and_labels_to_nx()
+    G = amp.parse_tasks_and_cores_to_nx()
+    #print(G.nodes(data=True))
+    #print(G.edges(data=True))
+
+    a = nxamp.NxConverter(G)
+    s = a.get_cpa_sys()
+
+    task_results = analysis.analyze_system(s)
+    print("Result:")
+    for r in sorted(s.resources, key=str):
+        for t in sorted(r.tasks, key=str):
+            print("%s: wcrt=%d" % (t.name, task_results[t].wcrt))
+            print("    b_wcrt=%s" % (task_results[t].b_wcrt_str()))
+
+if __name__ == '__main__':
+    test()
